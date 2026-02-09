5a6dacb524d5a064abc4190512dda9a73dce5bd0
Jayesh Patel <jayeshmepani09@gmail.com>
2026-02-09 23:10:02 +0530

    update

--- assets/js/video.js
+++ assets/js/video.js
@@ -18,7 +18,7 @@ function showControls(i) {
   controlTimeouts[videoId] = setTimeout(() => {
     i.classList.remove("show-controls");
     delete controlTimeouts[videoId];
-  }, 3000);
+  }, 1000);
 }
 
 function loadVideosInBackground() {
